3ace9e05333113f5ec4c7fff9dab60dc3d1d52ed
Brick Thornton <brickthornton@gmail.com>
2014-04-30 10:39:05 -0700

    updated external CSS stylesheet

--- source/bootbadge/app/models/user.rb
+++ source/bootbadge/app/models/user.rb
@@ -1,14 +1,25 @@
 class User < ActiveRecord::Base
+
   # Remember to create a migration!
   has_many :slogans
   belongs_to :cohort
 
-  def previous
-    self.class.order("id DESC").where("id < ?", self.id).first || self.class.last
+
+# create an array of users
+# find out the current index
+# return the ID of the next element in the array
+
+  def previous_id
+    cohort_students = self.cohort.users
+    current_index = cohort_students.index(self)
+    (cohort_students[current_index - 1] || self.class.last).id
   end
 
-  def next
-    self.class.order("id ASC").where("id > ?", self.id).first || self.class.first
+  def next_id
+    cohort_students = self.cohort.users
+    current_index = cohort_students.index(self)
+    (cohort_students[current_index + 1] || self.class.first).id
   end
 
 end
+
