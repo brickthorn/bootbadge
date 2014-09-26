e5a4cecfde1492d6ae87ea91e394c0061b6f315a
Brick Thornton <brickthornton@gmail.com>
2014-09-26 16:09:09 -0700

    removed comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,17 @@
 class User < ActiveRecord::Base
 
-  # Remember to create a migration!
   has_many :slogans
   belongs_to :cohort
-
-
-# create an array of users
-# find out the current index
-# return the ID of the next element in the array
-
 
   def previous_id
     cohort_students = self.cohort.users
     current_index = cohort_students.index(self)
-    # (cohort_students[current_index - 1] || self.class.last).id
     (cohort_students[current_index - 1] || cohort_students.last).id
   end
 
   def next_id
     cohort_students = self.cohort.users
     current_index = cohort_students.index(self)
-    # (cohort_students[current_index + 1] || self.class.first).id
     (cohort_students[current_index + 1] || cohort_students.first).id
   end
 
